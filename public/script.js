a8a38cf35c28629a540a7a480896b80add58c927
mukund04 <mukund.arya19@gmail.com>
2021-04-22 23:30:40 +0530

    first version

---
+++ public/script.js
@@ -0,0 +1,37 @@
+let loc = document.getElementById("location");
+let tempicon = document.getElementById("temp-icon");
+let tempvalue = document.getElementById("temp-value");
+let climate = document.getElementById("climate");
+let iconfile;
+
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cacc9e459d950c19ba2dc2c4151fed27`;
+      fetch(api)
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+
+        .then((data) => {
+          const { name } = data;
+          console.log(data);
+          const { feels_like } = data.main;
+          const { id, main } = data.weather[0];
+
+          loc.textContent = name;
+          climate.textContent = main;
+          tempvalue.textContent = Math.round(feels_like - 273);
+        });
+    });
+  }
+});
