a5f99a1762bc6e362fe4793e692dbaf55266a511
mukund04 <mukund.arya19@gmail.com>
2021-04-23 13:39:09 +0530

    Optimised for searching city

--- public/script.js
+++ public/script.js
@@ -7,6 +7,45 @@ let iconfile;
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(searchInput.value);
+  searchInput.value = "";
+});
+
+const getWeather = async (city) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cacc9e459d950c19ba2dc2c4151fed27`,
+
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    const { name } = weatherData;
+    const { feels_like } = weatherData.main;
+    const { id, main } = weatherData.weather[0];
+    loc.textContent = name;
+    climate.textContent = main;
+    tempvalue.textContent = Math.round(feels_like - 273);
+    if (id < 300 && id > 200) {
+      tempicon.src = "icons/thunder.png";
+    } else if (id < 400 && id > 300) {
+      tempicon.src = "icons/clouds.png";
+    } else if (id < 600 && id > 500) {
+      tempicon.src = "icons/rain.png";
+    } else if (id < 700 && id > 600) {
+      tempicon.src = "icons/snow.png";
+    } else if (id < 800 && id > 700) {
+      tempicon.src = "icons/clouds.png";
+    } else if (id == 800) {
+      tempicon.src = "icons/clear.png";
+    }
+  } catch (err) {
+    alert("city not found");
+  }
+};
+
 window.addEventListener("load", () => {
   let long;
   let lat;
@@ -23,13 +62,27 @@ window.addEventListener("load", () => {
 
         .then((data) => {
           const { name } = data;
-          console.log(data);
+
           const { feels_like } = data.main;
           const { id, main } = data.weather[0];
 
           loc.textContent = name;
           climate.textContent = main;
           tempvalue.textContent = Math.round(feels_like - 273);
+          if (id < 300 && id > 200) {
+            tempicon.src = "icons/thunder.png";
+          } else if (id < 400 && id > 300) {
+            tempicon.src = "icons/clouds.png";
+          } else if (id < 600 && id > 500) {
+            tempicon.src = "icons/rain.png";
+          } else if (id < 700 && id > 600) {
+            tempicon.src = "icons/snow.png";
+          } else if (id < 800 && id > 700) {
+            tempicon.src = "icons/clouds.png";
+          } else if (id == 800) {
+            tempicon.src = "icons/clear.png";
+          }
+          console.log(data);
         });
     });
   }
